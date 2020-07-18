e51bdf1721fba418419da0c85bffb6ed0fa182f8
rafaelhmdribeiro <rafaelribeiro@outlook.com>
2020-07-18 14:39:31 -0300

    Script de instalação das ferramentas

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo yum install -y https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce docker-ce-cli containerd.io
+
+sudo systemctl start docker.service
+
+sudo systemctl enable docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo yum install git -y
