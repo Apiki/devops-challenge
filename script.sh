880759be98a6a08ea4cb24af24cd93a8f336080e
rafaelhmdribeiro <30526561+rafaelhmdribeiro@users.noreply.github.com>
2020-07-19 18:58:21 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum install -y docker-ce docker-ce-cli containerd.io
 
 sudo systemctl start docker.service
 
