696154ca86881a76c7eb7cb81ef0ef6be273164a
Felipe Moraes <80363113+Felipe0599@users.noreply.github.com>
2022-08-01 19:16:46 -0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAZNYTHS6SD43I3EVH"
-    secret_key = "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"
+    access_key = "{Chave de Acesso IAM}"
+    secret_key = "{Chave de Acesso IAM}"
 }
